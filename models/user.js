e3965067057f338d023c769b295b57956250e815
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-10-02 20:32:35 -0600

    initial commit

---
+++ models/user.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail:true
+      },
+      unique: {
+          args: true,
+          msg: 'This email is already being used.'
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    api_key: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
