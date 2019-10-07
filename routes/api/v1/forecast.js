abad2a97f86ee82210be5e8b4c35152fbe8c98d3
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-10-06 23:28:28 -0600

    Adds UserForecast

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -4,48 +4,62 @@ const dotenv = require('dotenv');
 dotenv.config();
 var https = require('https');
 const fetch = require('node-fetch');
-const Bluebird = require('bluebird');
+var User = require('../../../models').User;
 
 // fetch.Promise = Bluebird;
 // var forecast = require('../../../services/google_api.js')
-var User = require('../../../models').User;
+
 
 router.get('/', function(req, res, next){
   res.setHeader("Content-Type", "application/json");
-   grab_coords(req.query.location)
+  User.findOne({ where: { api_key: req.body.api_key } })
+  .then( user => {
+    if(user)
+    {
+      grab_coords(req.query.location)
+    }
+    else
+    {
+      res.status(500).send({ error })
+    }
+    }
+  )
 
-  function grab_coords(place) {
-  var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${place}&key=${process.env.GOOGLE_KEY}`;
-  var headers = {
-    "Content-Type": "application/json",
-  }
-  fetch(url, { method: 'GET'})
-    .then((res) => {
-       return res.json()
-  })
-    .then((json) => {
-      coordinates = {
-      lat: json.results[0].geometry.location.lat,
-      lng: json.results[0].geometry.location.lng }
-      var url2 = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${String(coordinates.lat)},${String(coordinates.lng)}`;
-      fetch(url2, { method: 'GET'})
-        .then((res) => {
-           return res.json()
-      })
-      .then((json) => {
-         weather = {
-        location: req.query.location,   
-        currently: json.currently,
-        hourly: json.hourly,
-        daily: json.daily}
 
-        res.status(200).send(JSON.stringify(weather))
-      }
 
-    ).catch(error => res.status(500).send({ error }) )
-
+    function grab_coords(place) {
+    var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${place}&key=${process.env.GOOGLE_KEY}`;
+    var headers = {
+      "Content-Type": "application/json",
+    }
+    fetch(url, { method: 'GET'})
+      .then((res) => {
+         return res.json()
     })
-  }
+      .then((json) => {
+        coordinates = {
+        lat: json.results[0].geometry.location.lat,
+        lng: json.results[0].geometry.location.lng }
+        var url2 = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${String(coordinates.lat)},${String(coordinates.lng)}`;
+        fetch(url2, { method: 'GET'})
+          .then((res) => {
+             return res.json()
+        })
+        .then((json) => {
+           weather = {
+          location: req.query.location,
+          currently: json.currently,
+          hourly: json.hourly,
+          daily: json.daily}
+
+          res.status(200).send(JSON.stringify(weather))
+        }
+
+      ).catch(error => res.status(500).send({ error }) )
+
+      })
+    }
 })
+
 
 module.exports = router
